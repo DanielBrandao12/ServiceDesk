31043de0fe2f6882215abf096a64426a0434adef
Daniel Rodrigues Brandão <daniel.brandao@fatec.br>
2025-12-12 13:59:40 -0300

    Ajustes no card

--- frontend/src/pages/cardsInfo/index.tsx
+++ frontend/src/pages/cardsInfo/index.tsx
@@ -59,7 +59,7 @@ const CardsInfo = () => {
     useEffect(() => {
         const intervalo = setInterval(() => {
             setIndiceAtual((i) => {
-                const proximo = i + 3;
+                const proximo = i + 4;
                 return proximo >= metricas.length ? 0 : proximo;
             });
         }, 15000);
@@ -68,30 +68,30 @@ const CardsInfo = () => {
     }, [metricas.length]);
 
     // Determina os 4 itens da página atual
-    const cardsVisiveis = metricas.slice(indiceAtual, indiceAtual + 3);
+    const cardsVisiveis = metricas.slice(indiceAtual, indiceAtual + 4);
 
     return (
         <div className="flex flex-row w-full h-[100vh]">
             <iframe
             
                 src="https://10.68.96.5/public/mapshow.htm?id=2954&mapid=AF761232-767F-4DC0-A958-DE26C5F0B297"
-                className="w-[93%] h-[100%]"
+                className="w-[85%] h-[100%]"
 
             ></iframe>
             {
                 carregando ? (<p className="text-lg font-medium">Carregando dados do dashboard...</p>) :
                     (
 
-                        <div className="flex flex-col gap-6 justify-center items-center w-[7%] bg-[#242423]">
+                        <div className="flex flex-col gap-6 justify-center items-center w-[15%] bg-[#242423]">
 
 
                             {/* ====== MÉTRICAS ====== */}
-                            <div className="flex flex-col gap-8 mr-2">
+                            <div className="flex flex-col gap-8 mr-2 ">
 
                                 {cardsVisiveis.map((item, index) => (
                                     <div
                                         key={index}
-                                        className="flex flex-col items-center justify-center w-[130px]  h-[235px]  bg-[#242423] m-2 rounded-xl shadow-sm border border-gray-200 transition-all duration-300"
+                                        className="flex flex-col items-center justify-center w-[235px]  h-[235px]  bg-[#242423] m-2 rounded-xl shadow-sm border border-gray-200 transition-all duration-300"
                                     >
                                         <div className="mb-2">{item.icone}</div>
                                         <h3 className="text-lg font-medium text-gray-200 text-center p-4">
